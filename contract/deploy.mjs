f143301b7a7cdfc64515bcd9d6b90355c92e9874
PBillingsby <philbillingsby@gmail.com>
2022-09-18 16:08:49 -0400

    add working contract and deploy function

--- contract/deploy.mjs
+++ contract/deploy.mjs
@@ -2,23 +2,27 @@ import fs from 'fs'
 import { WarpFactory } from 'warp-contracts'
 
 async function deploy() {
+  // Creating a mainnet Warp instance
   const warp = WarpFactory.forMainnet();
+  // Importing what is needed for the initial deployment
   const wallet = JSON.parse(fs.readFileSync('wallet.json', 'utf-8'))
-  const state = fs.readFileSync('contract/state.json', 'utf-8')
   const contractSource = fs.readFileSync('contract/contract.js', 'utf-8')
+
+  // Setting contract initState & Deploying first contract
   const { contractTxId } = await warp.createContract.deploy({
     wallet,
-    initState: state,
+    initState: JSON.stringify({
+      "clicks": 0
+    }),
     src: contractSource
   })
 
-  fs.writeFileSync('../contractID.js', `export const contractID = "${contractTxId}"`)
-
   const contract = warp.contract(contractTxId).connect(wallet)
-
+  // Interacting with smart contract by calling the click function
   await contract.writeInteraction({
     function: 'click'
   })
+  // Reading and abstracting contract state
   const { cachedValue } = await contract.readState()
 
   console.log('Contract state: ', cachedValue)
