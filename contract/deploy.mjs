107f9b86fb3ae47833403bc280c8436654f59d68
PBillingsby <philbillingsby@gmail.com>
2022-09-19 16:39:09 -0400

    add arlocal tests

--- contract/deploy.mjs
+++ contract/deploy.mjs
@@ -1,11 +1,19 @@
 import fs from 'fs'
 import { WarpFactory } from 'warp-contracts'
 
-async function deploy() {
-  // Creating a mainnet Warp instance
-  const warp = WarpFactory.forMainnet();
-  // Importing what is needed for the initial deployment
-  const wallet = JSON.parse(fs.readFileSync('wallet.json', 'utf-8'))
+export async function deploy() {
+  // Creating local || mainnet Warp instance
+  const warp = process.env.NODE_ENV === "development" ? WarpFactory.forLocal() : WarpFactory.forMainnet();
+  // Creating test wallet || importing Arweave wallet from root
+  let wallet
+  if (process.env.NODE_ENV === "development") {
+    const testWallet = await warp.testing.generateWallet()
+    wallet = testWallet.jwk
+  }
+  else {
+    wallet = JSON.parse(fs.readFileSync('wallet.json', 'utf-8'))
+  }
+  // Importing the smart contract
   const contractSource = fs.readFileSync('contract/contract.js', 'utf-8')
 
   // Setting contract initState & Deploying first contract
@@ -22,10 +30,10 @@ async function deploy() {
   await contract.writeInteraction({
     function: 'click'
   })
+
   // Reading and abstracting contract state
   const { cachedValue } = await contract.readState()
 
-  console.log('Contract state: ', cachedValue)
-  console.log('contractTxId: ', contractTxId)
+  return { contractTxId, cachedValue }
 }
 deploy()
